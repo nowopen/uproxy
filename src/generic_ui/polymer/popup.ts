f142156c867453968fab6e7e95fd976c5c59e131
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2014-10-15 16:56:19 -0400

    refactor to make changeLanguage global

--- src/generic_ui/polymer/popup.ts
+++ src/generic_ui/polymer/popup.ts
@@ -2,3 +2,37 @@
 
 console.log('global model is: ', model);
 // ui['model'] = model;
+
+/**
+  * Return the language of the user's browser.
+  */
+// TODO (lucyhe): find a better way to do this.
+var getBrowserLanguage = () : string => {
+  return navigator.language.substring(0, 2);
+}
+
+
+// Translation.
+
+/** Retrieve messages.json file of the appropriate language and insert
+  * strings into the application's UI.  
+  */
+var changeLanguage = (language:string) => {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET','../locales/' + language + '/messages.json',true);
+  xhr.onload = function() {
+    if (this.readyState != 4) {
+      return;
+    }
+    // Translate the JSON format to a simple
+    // { key : value, ... } dictionary.
+    var translations = JSON.parse(xhr.responseText);
+    for (var key in translations) {
+      if (translations.hasOwnProperty(key)) {
+        translations[key] = translations[key].message;
+      }
+    }
+    i18nTemplate.process(document, translations);
+  }
+  xhr.send(null);  
+}
--- src/interfaces/ui-polymer.d.ts
+++ src/interfaces/ui-polymer.d.ts
@@ -6,3 +6,11 @@
 
 // TODO(#345): Actually have typescript for polymer.
 declare var Polymer :any;
+
+// For internationalization.
+interface I18n {
+  process :(element:Document, translations:any) => void;
+}
+declare var i18nTemplate :I18n;
+declare var getBrowserLanguage : () => string;
+declare var changeLanguage : (language:string) => void;
